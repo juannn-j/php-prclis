4f2b7040ab876f5d66e6178b222d8aee0be33491
juannn-j <lainfhj@proton.me>
2025-07-11 22:52:48 -0500

    create tables

---
+++ script.sql
@@ -0,0 +1,32 @@
+CREATE TABLE usuario (
+    idusuario SERIAL PRIMARY KEY,
+    correo VARCHAR(50) NOT NULL,
+    passwd VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE proyecto (
+    idproyecto SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    fechainicio DATE NOT NULL,
+    fechafinal DATE NOT NULL
+);
+
+CREATE TABLE cliente (
+    idcliente SERIAL PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+);
+
+CREATE TABLE asignacion (
+    idasignacion SERIAL PRIMARY KEY,
+    idproyecto INT NOT NULL,
+    idcliente INT NOT NULL,
+    CONSTRAINT fk_proyecto_cliente
+        FOREIGN KEY (idproyecto)
+        REFERENCES proyecto(idproyecto),
+    CONSTRAINT fk_cliente_proyecto
+        FOREIGN KEY (idcliente)
+        REFERENCES cliente(idcliente)
+);
